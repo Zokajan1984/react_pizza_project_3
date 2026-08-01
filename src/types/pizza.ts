7bd49d9ccb7cfc5367160f47b64ff28f292aa0ab
Zokajan <zokirjon.astanov84@gmail.com>
2026-08-01 12:02:18 +0500

    Добавлены типы данных: Category, Product, CartItem, Order

---
+++ src/types/pizza.ts
@@ -0,0 +1,63 @@
+import { Ingrid_Darling } from "next/font/google";
+
+export type DoughType = 0 | 1;
+
+export interface ProductVarinat {
+  size: number;
+  type: DoughType;
+  price: number;
+}
+
+export interface Category {
+  id: number;
+  nama: string;
+}
+
+export interface Product {
+  id: number;
+  categoryId: number;
+  name: string;
+  imageUrl: string;
+  variants: ProductVarinat[];
+  ingredients?: string[];
+}
+
+export interface CartItem {
+  cartItemId: string;
+  productId: number;
+  name: string;
+  imageUrl: string;
+  size: number;
+  type: DoughType;
+  price: number;
+  quantity: number;
+}
+
+export type OrderStatus =
+  | "new"
+  | "cooking"
+  | "delivering"
+  | "delivered"
+  | "cancelled";
+
+export interface OrderItem {
+  productId: number;
+  name: string;
+  imageUrl: string;
+  size: number;
+  type: DoughType;
+  price: number;
+  quantity: number;
+}
+
+export interface Order {
+  id: number;
+  items: OrderItem[];
+  phone: string;
+  name: string;
+  address?: string;
+  comment?: string;
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+}
